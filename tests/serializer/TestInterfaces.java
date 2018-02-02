153f0c4ad4a405da26a607e3e3765bf4a5f4882d
TheAIBot <andreasuranus@hotmail.com>
2018-02-02 23:12:29 +0100

    more tests!

--- tests/serializer/TestInterfaces.java
+++ tests/serializer/TestInterfaces.java
@@ -1,5 +1,36 @@
 package serializer;
 
-public class TestInterfaces {
+import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import testClasses.Animal;
+import testClasses.Cat;
+import testClasses.Dog;
+
+public class TestInterfaces {
+	@Test
+	public void serializeList() throws Exception {
+		List<Animal> expected = new ArrayList<Animal>();
+		expected.add(Dog.createRandomDog());
+		expected.add(Dog.createRandomDog());
+		expected.add(Dog.createRandomDog());
+		expected.add(Cat.createRandomCat());
+		expected.add(Dog.createRandomDog());
+		expected.add(Cat.createRandomCat());
+		
+		final List<Animal> actual = TestTools.deSerialize(List.class, expected);
+		
+		assertEquals(expected.size(), actual.size());
+		assertEquals(expected.get(0), actual.get(0));
+		assertEquals(expected.get(1), actual.get(1));
+		assertEquals(expected.get(2), actual.get(2));
+		assertEquals(expected.get(3), actual.get(3));
+		assertEquals(expected.get(4), actual.get(4));
+		assertEquals(expected.get(5), actual.get(5));
+		
+	}
 }
--- tests/serializer/TestPrimitives.java
+++ tests/serializer/TestPrimitives.java
@@ -1,5 +1,7 @@
 package serializer;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 
 import org.junit.Test;
@@ -10,16 +12,77 @@ import testClasses.Primitives;
 
 public class TestPrimitives {
 	@Test
-	public void testPrimitivesSerialization() throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException, IOException, NoValidConstructorException {
+	public void serializePrimitiveClass() throws Exception {
 		final Primitives expected = new Primitives(Byte.MAX_VALUE, Short.MAX_VALUE, Integer.MAX_VALUE, Long.MAX_VALUE, Float.MAX_VALUE, Double.MAX_VALUE, true, 'a');
 		final Primitives actual = TestTools.deSerialize(Primitives.class, expected);
 		expected.assertSame(actual);
 	}
 	
 	@Test
-	public void testPrimitiveWrappersSerialization() throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException, IOException, NoValidConstructorException {
+	public void serializePrimitiveWrappersClass() throws Exception {
 		final PrimitiveWrappers expected = new PrimitiveWrappers(Byte.MAX_VALUE, Short.MAX_VALUE, Integer.MAX_VALUE, Long.MAX_VALUE, Float.MAX_VALUE, Double.MAX_VALUE, true, 'a');
 		final PrimitiveWrappers actual = TestTools.deSerialize(PrimitiveWrappers.class, expected);
 		expected.assertSame(actual);
+	}
+	
+	@Test
+	public void serializeByte() throws Exception {
+		serializePrimitive(byte.class, (byte)53);
+		final Object obj = (byte)53;
+		serializePrimitive(Object.class, obj);
+	}
+
+	@Test
+	public void serializeShort() throws Exception {
+		serializePrimitive(short.class, (short)53);
+		final Object obj = (short)53;
+		serializePrimitive(Object.class, obj);
+	}
+
+	@Test
+	public void serializeInt() throws Exception {
+		serializePrimitive(int.class, (int)53);
+		final Object obj = (int)53;
+		serializePrimitive(Object.class, obj);
+	}
+
+	@Test
+	public void serializeLong() throws Exception {
+		serializePrimitive(long.class, (long)53);
+		final Object obj = (long)53;
+		serializePrimitive(Object.class, obj);
+	}
+
+	@Test
+	public void serializeFloat() throws Exception {
+		serializePrimitive(float.class, (float)53);
+		final Object obj = (float)53;
+		serializePrimitive(Object.class, obj);
+	}
+
+	@Test
+	public void serializeDouble() throws Exception {
+		serializePrimitive(double.class, (double)53);
+		final Object obj = (double)53;
+		serializePrimitive(Object.class, obj);
+	}
+
+	@Test
+	public void serializeBoolean() throws Exception {
+		serializePrimitive(boolean.class, (boolean)true);
+		final Object obj = (boolean)true;
+		serializePrimitive(Object.class, obj);
+	}
+
+	@Test
+	public void serializeCharacter() throws Exception {
+		serializePrimitive(char.class, (char)'3');
+		final Object obj = (char)'3';
+		serializePrimitive(Object.class, obj);
+	}
+	
+	private <T> void serializePrimitive(Class<T> clazz, T expected) throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException, IOException, NoValidConstructorException {
+		final T actual = TestTools.deSerialize(clazz, expected);
+		assertEquals(expected, actual);
 	}
 }
